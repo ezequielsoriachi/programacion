aaa13dfa14d7264d5db7f01c3a65eabb919bd3ce
AgustinEmanuel <148821261+AgustinEmanuel@users.noreply.github.com>
2023-10-31 10:17:07 -0300

    Add files via upload

--- menu.bat
+++ menu.bat
@@ -28,7 +28,42 @@ if %opcion% == 5 goto salir
 
 
 
-:manipulacion_de_directorios_agus
+@echo off
+set "ruta_red=\\nombre_de_la_maquina\compartido"
+
+REM Crear una carpeta en la máquina de red
+if not exist "%ruta_red%\nueva_carpeta" (
+    mkdir "%ruta_red%\nueva_carpeta"
+    echo Carpeta creada.
+) else (
+    echo La carpeta ya existe.
+)
+
+REM Eliminar una carpeta en la máquina de red
+if exist "%ruta_red%\carpeta_a_eliminar" (
+    rmdir /s /q "%ruta_red%\carpeta_a_eliminar"
+    echo Carpeta eliminada.
+) else (
+    echo La carpeta no existe.
+)
+
+REM Mover una carpeta en la máquina de red
+if exist "%ruta_red%\carpeta_origen" (
+    move "%ruta_red%\carpeta_origen" "%ruta_red%\carpeta_destino"
+    echo Carpeta movida.
+) else (
+    echo La carpeta de origen no existe.
+)
+
+REM Copiar una carpeta en la máquina de red
+if exist "%ruta_red%\carpeta_origen" (
+    xcopy /E /I "%ruta_red%\carpeta_origen" "%ruta_red%\carpeta_destino"
+    echo Carpeta copiada.
+) else (
+    echo La carpeta de origen no existe.
+)
+
+pause
 
 
 
