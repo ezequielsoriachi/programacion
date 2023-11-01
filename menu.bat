0f88bd2805eb9e88d0a474f8ea989f0892718cf3
ezequielsoriachi <83194544+ezequielsoriachi@users.noreply.github.com>
2023-11-01 20:50:11 -0300

    fusion1seba

--- menu.bat
+++ menu.bat
@@ -27,6 +27,50 @@ if %opcion% == 5 goto salir
 
 :scannear_red_seba
 
+:menu1
+del fichero*.tmp
+cls
+echo.
+echo  -----------------MENU1-----------------------
+echo  1. Escanear toda la red LAN
+echo  2. Salir
+echo.
+echo.
+set /p opc=Pulse el numero correspondiente para la accion requerida:
+if %opc%==1 goto scanner
+if %opc%==2 goto inicio
+
+
+:scanner
+cls
+echo Introduzca los 3 primeros octetos que forman la red
+echo (Ejemplo: 172.16.1)
+set /p ip=
+cls
+echo Escaneando toda la Red de area local (%ip%.0)
+echo -----------------------------------------------
+FOR /L %%a IN (1,1,254) DO (
+echo [ ESCANEANDO IP %%a de 254 ] - [%ip%.%%a]
+ping -w 50 -n 1 %ip%.%%a | find "Respuesta" && @ECHO IP Conectada --- [%ip%.%%a] >> fichero1.tmp
+)
+cls
+if exist fichero*.tmp (
+echo ---IPs Conectadas--- 
+echo.
+type fichero1.tmp
+echo.
+set /p ipe=Introduce la IP para conectarse:
+echo.
+echo.
+echo IP elegida con exito!!, pulse una tecla para salir
+del fichero*.tmp
+echo.
+pause>nul
+goto inicio
+) 
+echo Ip no encontradas, presione una tecla para escanear otra red
+pause>nul
+goto menu1
 
 
 :manipulacion_de_directorios_agus
