bf66a2f96398d975d1b70f0ae607e8991a7d8a40
ezequielsoriachi <83194544+ezequielsoriachi@users.noreply.github.com>
2023-11-08 21:18:09 -0300

    fusion miercoles 8 de noviembre

--- menu.bat
+++ menu.bat
@@ -75,7 +75,6 @@ goto menu1
 
 :manipulacion_de_directorios_agus
 
-
 @echo off
 echo.
 color a0
@@ -181,8 +180,8 @@ exit
 
 
 :manufiles
-
-color 0B
+set ruta=C:\W
+color 02
 title MANIPULACION DE ARCHIVOS
 cls
 echo.
@@ -204,36 +203,24 @@ set /p omf=
 	if %omf% == 3 (goto modificar)
 	if %omf% == 4 (goto mover)
 	if %omf% == 5 (goto eliminar)
-	if %omf% == 6 (goto bu)
+	if %omf% == 6 (goto bu1)
 	if %omf% == 7 (goto inicio)
 	if %omf% == 77 (goto m)
 	if %omf% neq 77 (goto ono)
-
 
 :listar
 title LISTAR ARCHIVOS
 cls
 echo.
-echo INGRESE EL DIRECTORIO QUE DESEA REVISAR: 
-echo.
-set /p dt1=
-cls
-echo.
 echo LOS ARCHIVOS EN ESTE DIRECTORIO SON: 
 echo.
-for /r "%dt1%" %%I in (*.*) do (echo %%I)
+for /r "%ruta%" %%I in (*.*) do (echo %%I)
 echo.
 pause>nul
 goto :manufiles
 
-
 :crear
 title CREAR ARCHIVO
-cls
-echo.
-echo INGRESE EL DIRECTORIO DONDE SE GUARDARA EL ARCHIVO:
-echo.
-set /p dt2= 
 cls
 echo.
 echo INGRESE EL NOMBRE DEL ARCHIVO:
@@ -250,14 +237,13 @@ echo INGRESE LA EXTENSION DEL ARCHIVO:
 echo.
 set /p e2= 
 cls
-echo %c2% > "%dt2%\%n2%.%e2%"
+echo %c2% > "%ruta%\%n2%.%e2%"
 cls
 echo.
 echo OPERACION COMPLETA, PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL...
 echo.
 pause>nul
 goto :manufiles
-
 
 :modificar
 title MODIFICAR ARCHIVO
@@ -270,29 +256,24 @@ echo 2. SOBREESCRIBIR ARCHIVO.
 echo.
 set /p mod= 
 	if %mod% == 1 (goto editar) 
-	if %mod% == 2 (goto sobre)	
+	if %mod% == 2 (goto sobre)
 	if %mod% neq 2 (goto ono)
 :editar
 cls
 echo.
-echo INGRESE EL DIRECTORIO DONDE SE ENCUENTRA EL ARCHIVO A EDITAR:
-echo.
-set /p dt3= 
-cls
-echo.
 echo LOS ARCHIVOS EN ESTE DIRECTORIO SON:
 echo.
-for /r "%dt3%" %%I in (*.*) do (echo %%I)
+for /r "%ruta%" %%I in (*.*) do (echo %%I)
 echo.
-echo INGRESE LA DIRECCION COMPLETA DEL ARCHIVO A EDITAR:
+echo INGRESE EL NOMBRE DEL ARCHIVO A EDITAR:
 echo.
-set /p dt33= 
+set /p dt3= 
 cls
 echo.
 echo INGRESE EL CONTENIDO A INTRODUCIR EN EL DOCUMENTO:
 echo.
 set /p cnt3= 
-echo %cnt3% >> "%dt33%"
+echo %cnt3% >> "%ruta%\%dt3%"
 cls
 echo.
 echo OPERACION COMPLETA, PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL...
@@ -302,112 +283,102 @@ goto :manufiles
 :sobre
 cls
 echo.
-echo INGRESE EL DIRECTORIO DONDE SE ENCUENTRA EL ARCHIVO A EDITAR:
-echo.
-set /p dt333= 
-cls
-echo.
 echo LOS ARCHIVOS EN ESTE DIRECTORIO SON:
 echo.
-for /r "%dt333%" %%I in (*.*) do (echo %%I)
+for /r "%ruta%" %%I in (*.*) do (echo %%I)
 echo.
-echo INGRESE LA DIRECCION COMPLETA DEL ARCHIVO A SOBREESCRIBIR:
+echo INGRESE EL NOMBRE DEL ARCHIVO A SOBREESCRIBIR:
 echo.
-set /p dt3333= 
+set /p dt33= 
 cls
 echo.
 echo INGRESE EL CONTENIDO A SOBREESCRIBIR EN EL DOCUMENTO:
 echo.
 set /p cnt33= 
-echo %cnt33% > "%dt3333%"
+echo %cnt33% > "%ruta%\%dt33%"
 cls
 echo.
 echo OPERACION COMPLETA, PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL...
 echo.
 pause>nul
 goto :manufiles
-
 
 :mover
 title MOVER ARCHIVO
 cls
 echo.
-echo INGRESE EL DIRECTORIO DONDE SE ENCUENTRA EL ARCHIVO A MOVER:
-echo.
-set /p dt4= 
-cls
-echo.
 echo LOS ARCHIVOS EN ESTE DIRECTORIO SON:
 echo.
-for /r "%dt4%" %%I in (*.*) do (echo %%I)
+for /r "%ruta%" %%I in (*.*) do (echo %%I)
 echo.
-echo INGRESE LA DIRECCION COMPLETA DEL ARCHIVO A MOVER:
+echo INGRESE EL NOMBRE DEL ARCHIVO A MOVER:
 echo.
 set /p dt44= 
+cls 
+echo.
+echo LOS DIRECTORIOS Y ARCHIVOS SON:
+echo.
+cd %ruta%
+dir /b
 echo.
 echo INGRESE EL DIRECTORIO DONDE SE GUARDARA EL ARCHIVO A MOVER:
 echo.
 set /p dt444= 
-move "%dt44%" "%dt444%"
+move "%ruta%\%dt44%" "%ruta%\%dt444%"
 cls
 echo.
 echo OPERACION COMPLETA, PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL...
 echo.
+cls
+echo.
+for /r "%ruta%" %%I in (*.*) do (echo %%I)
+echo.
 pause>nul
 goto :manufiles
-
 
 :eliminar
 title ELIMINAR ARCHIVO
 cls
 echo.
-echo INGRESE EL DIRECTORIO DONDE SE ENCUENTRA EL ARCHIVO A ELIMINAR:
-echo.
-set /p dt5= 
-cls
-echo.
 echo LOS ARCHIVOS EN ESTE DIRECTORIO SON:
 echo.
-for /r "%dt5%" %%I in (*.*) do (echo %%I)
+for /r "%ruta%" %%I in (*.*) do (echo %%I)
 echo.
-echo INGRESE LA DIRECCION COMPLETA DEL ARCHIVO A ELIMINAR:
+echo INGRESE EL NOMBRE DEL ARCHIVO A ELIMINAR:
 echo.
-set /p dt55= 
-	if exist "%dt55%" (del "%dt55%")
+set /p dt5= 
+	if exist "%ruta%\%dt5%" (del "%ruta%\%dt5%")
 cls
+echo %ruta%\%dt5%
 echo.
 echo OPERACION COMPLETA, PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL...
 echo.
 pause>nul
 goto :manufiles
 
-
-:bu
+:bu1
 title RESPALDAR ARCHIVO
-cls
-echo.
-echo INGRESE EL DIRECTORIO DONDE SE ENCUENTRA EL ARCHIVO A RESPALDAR:
-echo.
-set /p dt7= 
 cls
 echo.
 echo LOS ARCHIVOS EN ESTE DIRECTORIO SON:
 echo.
-for /r "%dt7%" %%I in (*.*) do (echo %%I)
+for /r "%ruta%" %%I in (*.*) do (echo %%I)
 echo.
-echo INGRESE LA DIRECCION COMPLETA DEL ARCHIVO A RESPALDAR:
+echo INGRESE EL NOMBRE DEL ARCHIVO A RESPALDAR:
 echo.
 set /p dt77= 
 cls
 echo.
-echo INGRESE EL DIRECTORIO DONDE SE GUARDARA EL ARCHIVO A RESPALDAR:
+	if exist "Backup" (goto bu2)
+	if exist "backup" (goto bu2)
+mkdir %ruta%\Backup
+:bu2
+copy "%ruta%\%dt77%" "%ruta%\Backup"
 echo.
-set /p dt777= 
-copy "%dt77%" "%dt777%"
+echo OPERACION COMPLETA, PRESIONE CUALQUIER TECLA PARA VOLVER AL MENU PRINCIPAL...
 echo.
 pause>nul
 goto :manufiles
-
 
 :m
 cls
@@ -416,7 +387,6 @@ echo                                           - PROGRAMA CREADO POR MANUEL FONT
 echo.
 pause>nul
 goto :manufiles
-
 
 :ono
 title MANIPULACION DE ARCHIVOS
